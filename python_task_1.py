30d3dda71a87d2249aaeba747830ba51f2c463fb
dheerajkumar0 <131248195+dheerajkumar0@users.noreply.github.com>
2023-12-08 21:15:55 +0530

    Update python_task_1.py

--- python_task_1.py
+++ python_task_1.py
@@ -13,6 +13,9 @@ def generate_car_matrix(df)->pd.DataFrame:
                           where 'id_1' and 'id_2' are used as indices and columns respectively.
     """
     # Write your logic here
+    car_matrix = df.pivot(index='id_1', columns='id_2', values='car').fillna(0)
+    car_matrix.values[[range(len(car_matrix))]*2] = 0
+    return car_matrix
 
     return df
 
@@ -28,7 +31,15 @@ def get_type_count(df)->dict:
         dict: A dictionary with car types as keys and their counts as values.
     """
     # Write your logic here
-
+    conditon = [(df['car'] <= 15),
+                (df['car']>15)&(df['car']<=25),
+                (['car']>25)]
+    choices = ['low','medium','high']
+    df['car_type'] = pd.Series(np.select(condition, choices, default='unknown'), dtpye='category')
+    type_count = df['car_type'].value_counts().to_dict()
+    sorted_type_count = dict(sorted(type_count.items()))
+    return sorted_type_count
+    
     return dict()
 
 
@@ -43,6 +54,12 @@ def get_bus_indexes(df)->list:
         list: List of indexes where 'bus' values exceed twice the mean.
     """
     # Write your logic here
+    
+    bus_mean = df['bus'].mean()
+    bus_indexes= df[df['bus']>2*bus_mean].index.tolist()
+    bus_indexes.sort()
+    return bus_indexes
+    
 
     return list()
 
@@ -58,6 +75,10 @@ def filter_routes(df)->list:
         list: List of route names with average 'truck' values greater than 7.
     """
     # Write your logic here
+    
+    rout_avg_truck = df.groupby('route')[route_avg_truck > 7].index.tolist()
+    filtered_routes.sort()
+    return filtered_routes
 
     return list()
 
@@ -73,7 +94,13 @@ def multiply_matrix(matrix)->pd.DataFrame:
         pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
     """
     # Write your logic here
-
+    
+    modified_matrix = car_matrix.copy()
+    modified_matrix[modified_matrix > 20] *= 0.75
+    modified_matrix[modified_matrix <= 20] *= 1.25
+    modified_matrix = modified_matrix.round(1)
+    return modified_matrix
+    
     return matrix
 
 
